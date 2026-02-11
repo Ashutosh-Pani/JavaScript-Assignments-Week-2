e5ac32a6e7d7b9109ba940635fa4038f1d31907f
Ashutosh <ashutoshpani1111@gmamil.com>
2026-02-11 09:02:08 +0530

    complete 2-clock

--- 01-async-js/medium/2-clock.js
+++ 01-async-js/medium/2-clock.js
@@ -1,8 +1,19 @@
 // Using `1-counter.md` or `2-counter.md` from the easy section, can you create a
 // clock that shows you the current machine time?
 
-// Can you make it so that it updates every second, and shows time in the following formats - 
+// Can you make it so that it updates every second, and shows time in the following formats -
 
 //  - HH:MM::SS (Eg. 13:45:23)
 
 //  - HH:MM::SS AM/PM (Eg 01:45:23 PM)
+
+function clock() {
+  const time = new Date();
+  const hours = String(time.getHours()).padStart(2, "0");
+  const min = String(time.getMinutes()).padStart(2, "0");
+  const sec = String(time.getSeconds()).padStart(2, "0");
+  const ampm = hours >= 12 ? "AM" : "PM";
+  console.clear();
+  console.log(`${hours} : ${min} : ${sec} ${ampm}`);
+}
+setInterval(clock, 1000);
