bad702bb84be6fa4a7866a406cace863bd3f5048
Ashutosh <ashutoshpani1111@gmamil.com>
2026-02-11 07:02:26 +0530

    fourth Problem solved

--- 01-async-js/easy/4-write-to-file.js
+++ 01-async-js/easy/4-write-to-file.js
@@ -2,3 +2,10 @@
 
 // Using the fs library again, try to write to the contents of a file.
 // You can use the fs library to as a black box, the goal is to understand async tasks.
+const fs = require("fs");
+
+fs.writeFile("1-counter.js", "hello", (err) => {
+  if (err) {
+    console.log("something went wrong");
+  }
+});
