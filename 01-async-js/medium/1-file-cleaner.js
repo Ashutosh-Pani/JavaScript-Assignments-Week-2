2b73d68524dcc9f5c1b1173189fd6564bdedbd17
Ashutosh <ashutoshpani1111@gmamil.com>
2026-02-11 07:59:59 +0530

    solved 1-fileCleaner

--- 01-async-js/medium/1-file-cleaner.js
+++ 01-async-js/medium/1-file-cleaner.js
@@ -11,3 +11,17 @@
 // ```
 // hello world my name is raman
 // ```
+
+const fs = require("fs").promises;
+
+async function cleanFile() {
+  try {
+    const data = await fs.readFile(path, "utf-8");
+    const cleaned = data.replace(/\s+/g, " ").trim();
+    await fs.writeFile(path, cleaned);
+  } catch (err) {
+    console.log("Error:", err.message);
+  }
+}
+
+cleanFile();
