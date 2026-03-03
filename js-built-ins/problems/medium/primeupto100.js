8d17d02a791166f35214f73b4d5328f3c1ba688d
Ashutosh <ashutoshpani1111@gmamil.com>
2026-03-03 18:42:09 +0530

    prime complete

--- js-built-ins/problems/medium/primeupto100.js
+++ js-built-ins/problems/medium/primeupto100.js
@@ -16,7 +16,24 @@
 */
 
 function getPrimesUpTo100() {
-  // Your code here
+  function isPrime(num){
+    if(num<=1) return false;
+    if(num == 2) return true;
+
+    for(let i = 2; i<= Math.sqrt(num); i++){
+     if (num % i === 0) return false;
+    }
+    return true;
+    
+  }
+  const isPrimeArr = []
+
+  for(let i =2 ; i<=100; i++){
+    if(isPrime(i)){
+      isPrimeArr.push(i);
+    }
+}
+return isPrimeArr;
 }
 
 module.exports = { getPrimesUpTo100 };
